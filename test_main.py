61500935ac00522bec9ac77234a87edc00b9b5cf
haynec <haynec@uw.edu>
2025-02-23 21:23:48 -0800

    maybe fixed

--- test_main.py
+++ test_main.py
@@ -1,14 +1,11 @@
 import subprocess
-from unittest.mock import patch
 
 def run_main_with_params(params):
-    result = subprocess.run(['python', 'main.py', '--params-file', 'quadsim/params/cinema_vp.py', '--params', params], capture_output=True, text=True)
+    result = subprocess.run(['python', 'main.py', '--params-file', 'quadsim/params/cinema_vp.py', '--plotting', params], capture_output=True, text=True)
     assert result.returncode == 0, f"Process failed with return code {result.returncode}\n{result.stdout}\n{result.stderr}"
 
-@patch('main.plot_func')  # Mock the plotting function
-def test_trajectory(mock_plot_func):
+def test_trajectory():
     run_main_with_params('trajectory')
-    mock_plot_func.assert_called_once()
 
 # def test_camera_view():
 #     run_main_with_params('camera_view')
