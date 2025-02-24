0de4d43df5e162708dff9c4ca311739a56e93b42
haynec <haynec@uw.edu>
2025-02-23 21:07:30 -0800

    modified tests

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_main():
-    result = subprocess.run(['python', 'main.py', '--params-file', 'quadsim/params/cinema_vp.py', '--params-file', 'trajectory camera_view conic_view scp_iters constr_vio control losses state'], capture_output=True, text=True)
+    result = subprocess.run(['python', 'main.py', '--params-file', 'quadsim/params/cinema_vp.py', '--params-file', 'trajectory', 'camera_view', 'conic_view', 'scp_iters', 'constr_vio', 'control losses', 'state'], capture_output=True, text=True)
     assert result.returncode == 0, f"Process failed with return code {result.returncode}\n{result.stdout}\n{result.stderr}"
